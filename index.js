5e53b15d3da6cc7d98df0237fcc105e624636ad7
Kara Heeb <karaheeb@gmail.com>
2022-01-06 23:12:51 -0500

    added questions array for user input

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email?"
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What would you like to name your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please providea short description explain the what, why, and how of your project.'
+
+    },
+    {
+        type: 'checkbox',
+        name: 'contents',
+        message: 'Check the boxes to chose what to include in your README.md',
+        choices: [ 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Check the box to select your license of choice.',
+        choices: [ 'Apache 2.0', 'GNU General Public', 'MIT', 'The Unlicense', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for how other developers may contribute to your project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide tests for your application and how to run them.'
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: "Please provide instructions for how to contact you with any questions."
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

416d8562b63eabcac1125128fd16ef1274e63748
Kara Heeb <karaheeb@gmail.com>
2022-02-08 10:08:33 -0500

    updated table of content links

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -54,11 +54,12 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## Table of Contents
-    * [Installation](#Installation)
-    * [Usage](#Usage)
-    * [License](#License)
-    * [Contributing](#Contributing)
-    * [Tests](#Tests)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions] (#questions)
 
   ## Installation
   ${data.install}
